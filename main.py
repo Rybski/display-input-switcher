2a7f3ab211d484ffab6d76ad48c005715c5614a6
Rybski <michal.rylski98@gmail.com>
2023-08-01 18:43:34 +0200

    working prototype version

---
+++ main.py
@@ -0,0 +1,12 @@
+import monitorcontrol
+
+monitor_list : list = monitorcontrol.get_monitors()
+#print(monitor_list)
+
+for monitor in monitor_list:
+    with monitor as monitor_ctx:
+        #print(monitor_ctx.get_vcp_capabilities()) 
+        if(monitor_ctx.get_vcp_capabilities()["model"] == 'VG242Y P'):
+            #print(monitor_ctx.get_input_source())
+            monitor_ctx.set_input_source(17)                                # DP1->15, HDMI1->17, HDMI2->18
+            
